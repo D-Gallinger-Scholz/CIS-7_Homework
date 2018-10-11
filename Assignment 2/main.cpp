8597cec3c6f84805615d46fb65dd17478cfbd8b3
D-Gallinger-Scholz <42823821+D-Gallinger-Scholz@users.noreply.github.com>
2018-10-10 20:41:08 -0700

    Update main.cpp

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -3,109 +3,160 @@
 #include <cstdlib>
 
 using namespace std;
-int main(){
+int main()
+{
 	//create variables
   string formula;
-	string testValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZv^!->";
+	string testValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZv^!->()";
 	string testStatements = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
 	char lastCharacter;
+  int parenthesisCounter = 0;
 	bool isValidWFF = true;
 	bool isFirst = true;
 	bool isValidChar = false;
 	bool statementFollowsNegation = false;
   bool closingTime = true;
+  bool firstParenthesis = true;
   //cycle through the program until the user quits
-  while(closingTime == true){
+  while(closingTime == true)
+  {
 	  //ask user to input a formula
 	  cout << "Enter a formula or '0' to quit: ";
 	  getline(cin, formula);
 	  //cycle through the characters in the formula
-	  for(unsigned int i = 0; i<formula.length(); i++){
+	  for(unsigned int i = 0; i<formula.length(); i++)
+    {
 	  	//place the current character of the formula in a separate  variable
 	  	char current = formula[i];
       //if the user entered a zero, end the program
-     if(current == '0'){
+     if(current == '0')
+     {
         exit(0);
      }
+     //if someone was dumb enough to use a closing parenthesis b4 an starting parenthesis, end them!
+     if(current == ')' && firstParenthesis == true)
+     {
+       isValidWFF = false;
+       lastCharacter = current;
+       break;
+     }
+     //if an opening parenthesis is added first, all is right in the world, but remember to count it
+     if(current == '(')
+     {
+       firstParenthesis = false;
+       parenthesisCounter++;
+     }
+     //now count down the closing parentheses
+     if(current == ')')
+     {
+       parenthesisCounter--;
+     }
 	  	//make sure if there is a '-' then '>' follows it without any spaces
-	  	if(lastCharacter == '-' && current != '>'){
+	  	if(lastCharacter == '-' && current != '>')
+      {
 	  		isValidWFF = false;
         lastCharacter = current;
 	  		break;
 	  	}
-	  	else if(lastCharacter == '-' && current == '>'){
+	  	else if(lastCharacter == '-' && current == '>')
+      {
 	  		lastCharacter = current;
 	  		continue;
 	  	}
 	  	//ignore all other spaces
-		  if(current == ' '){
+		  if(current == ' ')
+      {
 		    	continue;
 	  	}
 	  	//make sure the current character is a valid formula component
-		  for(unsigned int j = 0; j<testValues.length(); j++){
+		  for(unsigned int j = 0; j<testValues.length(); j++)
+      {
 		  	char currentTest = testValues[j];
-		  	if(current == currentTest){
+		  	if(current == currentTest)
+        {
 		  		isValidChar = true;
 		  	}
 		  }
-		  if(!isValidChar){
+		  if(!isValidChar)
+      {
 		  	isValidWFF = false;
         lastCharacter = current;
 		  	break;
 		  }
-	  	else{
+	  	else
+      {
 	  		isValidChar = false;
 	  	}
 		  //make sure the formula doesn't begin with a connector
-		  if(isFirst){
-		  	if(current == '-' || current == 'v' || current == 'V' ||  current == '^'){
+		  if(isFirst)
+      {
+		  	if(current == '-' || current == 'v' || current == 'V' ||  current == '^')
+        {
 		  		isValidWFF = false;
          lastCharacter = current;
 			  	break;
 		  	}
-		  	else{
+		  	else
+        {
 		  		isFirst = false;
 		  	}
 		  }
 		  //make sure a connector isn't followed by another connector, except '!'
-		  if(lastCharacter == '>' || lastCharacter == 'v' ||  lastCharacter == 'V' || lastCharacter == '^'){
-		  	if(current == '-' || current == 'v' || current == 'V' ||  current == '^'){
+		  if(lastCharacter == '>' || lastCharacter == 'v' ||  lastCharacter == 'V' || lastCharacter == '^')
+      {
+		  	if(current == '-' || current == 'v' || current == 'V' ||  current == '^')
+        {
 			  	isValidWFF = false;
          lastCharacter = current;
 			  	break;
 		  	}
 		  }
 		  //make sure '!' is followed by a statement
-		  if(lastCharacter == '!'){
-		  	for(unsigned int k = 0; k<testStatements.length(); k++){
+		  if(lastCharacter == '!')
+      {
+		  	for(unsigned int k = 0; k<testStatements.length(); k++)
+        {
 			  	char currentStatement = testStatements[k];
-			  	if(current == currentStatement){
+			  	if(current == currentStatement)
+          {
 			  		statementFollowsNegation = true;
 			  	}
 		  	}
-		    if(!statementFollowsNegation){
+		    if(!statementFollowsNegation)
+        {
 			    isValidWFF = false;
          lastCharacter = current;
 		    	break;
 		    }
-	  	  else{
+	  	  else
+        {
 	  	  	statementFollowsNegation = false;
 	  	  }
 	  	}
       lastCharacter = current;
     }
 	  //make sure the formula doesn't end with a connector
-	  if(lastCharacter == '>' || lastCharacter == 'v' || lastCharacter == 'V' || lastCharacter == '^' || lastCharacter == '!'){
+	  if(lastCharacter == '>' || lastCharacter == 'v' || lastCharacter == 'V' || lastCharacter == '^' || lastCharacter == '!')
+    {
 	  	isValidWFF = false;
 	  }
-	  if(isValidWFF){
+    //make sure the formula has opening and closing parentheses in equal numbers
+    if(parenthesisCounter != 0)
+    {
+      isValidWFF = false;
+      parenthesisCounter = 0;
+    }
+	  if(isValidWFF)
+    {
 	  	cout << "The formula is a WFF!" << endl << endl;
   	}
 	  //otherwise, state that it is not a WFF
-  	else{
+  	else
+    {
 	  	cout << "The formula is not a WFF you dummy!" << endl << endl;
   	}
     isValidWFF = true;
+    firstParenthesis = true;
   }
   return 0;
 }
